8d8496c2f538477bc936c0b2941cbcf870fa6365
Yunzi <katete051@naver.com>
2025-08-07 14:36:49 +0900

    오늘의 학습 api 구현 완료

--- routes/gptRoutes.js
+++ routes/gptRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+const gptController = require('../controllers/gptController');
 const {
   generateQuote,
   searchWordDefinition,
