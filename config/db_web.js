ac1cf4438b1a7a296c5f2419000f8b218235bee7
yewon20804 <128349522+yewon20804@users.noreply.github.com>
2025-10-28 11:21:49 +0900

    fix: match web DB environment variable names

--- config/db_web.js
+++ config/db_web.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
   ssl: { rejectUnauthorized: false }, // Render DB 필수!
 });
 
