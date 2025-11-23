6972dfe3ba95b6fa3177f705639c283641e282d6
Chea-yunzi <katete051@naver.com>
2025-11-23 17:02:35 +0900

    🐛 모델명 잘못 사용함. 수정함

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -90,7 +90,7 @@ async function callPerplexityChat(messages, { temperature = 0.7, max_tokens = 12
   return axios.post(
     'https://api.perplexity.ai/chat/completions',
     {
-      model: 'sonar-medium-chat',
+      model: 'sonar-pro',
       messages,
       temperature,
       max_tokens,
